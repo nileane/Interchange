5f918d4a37de3a320c3fb63a6c17338170bd2bac
Schoewilliam <schoewilliam@gmail.com>
2015-04-14 06:00:38 +0200

    Oh wait, that ID on body is useless :D

--- js/keybinds.js
+++ js/keybinds.js
@@ -10,30 +10,30 @@ document.onkeydown = function(e) {
     e = e || window.event;
     switch(e.which || e.keyCode) {
         case 37: // left
-            if (document.getElementById('body').hasClass('rig')) { // Press Left to go back to Homescreen from Right Pane
-                document.getElementById('body').className = 'hom';
-            } else if (document.getElementById('body').hasClass('hom')) { // Press Left to go to Left Pane
-                document.getElementById('body').className = 'lef';
+            if (document.body.hasClass('rig')) { // Press Left to go back to Homescreen from Right Pane
+                document.body.className = 'hom';
+            } else if (document.body.hasClass('hom')) { // Press Left to go to Left Pane
+                document.body.className = 'lef';
             }
         break;
 
         case 38: // up
-            if (document.getElementById('body').hasClass('bot')) { // Press Up to go back to Homescreen from Bottom Pane
-                document.getElementById('body').className = 'hom';
+            if (document.body.hasClass('bot')) { // Press Up to go back to Homescreen from Bottom Pane
+                document.body.className = 'hom';
             }
         break;
 
         case 39: // right
-            if (document.getElementById('body').hasClass('lef')) { // Press Right to go back to Homescreen from Left Pane
-                document.getElementById('body').className = 'hom';
-            } else if (document.getElementById('body').hasClass('hom')) {
-                document.getElementById('body').className = 'rig'; // Press Righ to go to Right Pane
+            if (document.body.hasClass('lef')) { // Press Right to go back to Homescreen from Left Pane
+                document.body.className = 'hom';
+            } else if (document.body.hasClass('hom')) {
+                document.body.className = 'rig'; // Press Righ to go to Right Pane
             }
         break;
 
         case 40: // down
-            if (document.getElementById('body').hasClass('hom')) { // Press Down to go to Bottom Pane
-                document.getElementById('body').className = 'bot';
+            if (document.body.hasClass('hom')) { // Press Down to go to Bottom Pane
+                document.body.className = 'bot';
             }
         break;
 
